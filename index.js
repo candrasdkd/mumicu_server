247dd209a09a8617c6b105da61b12e629dee28c4
candrasdkd <projectcsd07@gmail.com>
2022-07-27 23:26:40 +0000

    add http server

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import router from "./src/routes/index.js";
+import http from "http-server"
 dotenv.config();
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 7777
 
+const server = http.createServer(app)
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -14,4 +16,7 @@ app.use(cors({ credentials: true, origin: '*' }));
 app.use(cookieParser());
 app.use(router);
 
-app.listen(port, () => console.log(`Server running at port http://localhost:${port}`));
+server.listen(port,() => {
+    console.log(`this app is running on ${port}`)
+})
+

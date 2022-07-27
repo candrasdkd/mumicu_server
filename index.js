8fa5399376efeb0ee6ed3fa6db91c4dc8b67d6de
Candra Sidik Dermawan <projectcsd07@gmail.com>
2022-07-28 06:42:19 +0700

    remove http

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import router from "./src/routes/index.js";
-import http from "http-server"
 dotenv.config();
-const app = express();
-const port = process.env.PORT || 7777
 
-const server = http.createServer(app)
+const app = express();
+const port = process.env.PORT || 8000
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -16,7 +14,4 @@ app.use(cors({ credentials: true, origin: '*' }));
 app.use(cookieParser());
 app.use(router);
 
-server.listen(port,() => {
-    console.log(`this app is running on ${port}`)
-})
-
+app.listen(port, () => console.log(`Server running at port http://localhost:${port}`));
